ffb5ad0e84e2a18752caada5f76d9c538cbe9a72
Maria1808 <mvbannikova1808@gmail.com>
2022-05-08 01:07:54 +0500

    Получение данных с сервера

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import { login } from './http/API';
+import { useEffect, useState } from 'react';
+
 
 function App() {
-    const test = () =>{
-        login()
+     
+    const [isxods, setIsxod] = useState(null)
+
+    function test(){
+        login().then(data=>setIsxod(data))
     }
+
   return (
     <div className="App">
+        {console.log(isxods.response[0].activity)}
       <header className="App-header">
         <button onClick={()=>test()}>fcgvhbjnk</button>
+        <label>{isxods.response[0].activity}</label>
       </header>
     </div>
   );
--- src/http/API.js
+++ src/http/API.js
@@ -3,7 +3,7 @@ import axios from "axios";
 let token = '1302382880b8e0c6b319eda1656dd1ad9a40d63f02fcb1e2b42f74b0c7c88b24e6411d474e56945989352'
 
 export const login = async () => {
-    const {data} = await axios.post('https://api.vk.com/method/groups.getById?v=5.131&fields=members_count&group_id=queenspublic&access_token='+token)
-    console.log(data.response[0].members_count)
+    const {data} = await axios.get('http://localhost:5000/api/user/')
+    console.log(data)
     return data
 }
